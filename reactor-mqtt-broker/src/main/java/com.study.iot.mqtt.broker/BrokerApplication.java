923e0ecd7658c6e677229e32460a4a15e21684c7
wuzguo <wuzguo@gmail.com>
2021-05-06 10:01:01 +0800

    broker

---
+++ reactor-mqtt-broker/src/main/java/com.study.iot.mqtt.broker/BrokerApplication.java
@@ -0,0 +1,20 @@
+package com.study.iot.mqtt.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <B>说明：描述</B>
+ *
+ * @author zak.wu
+ * @version 1.0.0
+ * @date 2021/5/6 9:54
+ */
+
+@SpringBootApplication
+public class BrokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrokerApplication.class, args);
+    }
+}
